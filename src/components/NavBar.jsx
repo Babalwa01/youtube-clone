9ab94bd6b2db640a1a0c6dfc74f6081de63628a3
Babalwa01 <babalwambolekwa93@gmail.com>
2023-05-18 06:13:01 +0200

    added home path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => (
     alignItems="center" 
     p={2} sx={{ position: "sticky", background: "#000", top: 0, justifyContent: "space-between" }}
   >
-    <Link to="/youtube-clone" style={{ display: "flex", alignItems: "center" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
     <SearchBar />
