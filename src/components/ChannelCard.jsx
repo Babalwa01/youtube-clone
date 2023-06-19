0a21f29a87044e671799e3dc3f1c76c65b6f927c
Babalwa01 <babalwambolekwa93@gmail.com>
2023-06-19 21:15:45 +0200

    added channel details and channel card

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
   return (
     <Box
       sx={{
@@ -13,9 +13,10 @@ const ChannelCard = ({ channelDetail }) => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        width: {xs: '356px', md: '320px'},
-        height: '326px',
-        margin: 'auto'
+        width: { xs: "356px", md: "320px" },
+        height: "326px",
+        margin: "auto",
+        marginTop
       }}
     >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -46,10 +47,13 @@ const ChannelCard = ({ channelDetail }) => {
             {channelDetail?.snippet?.title}
             <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
           </Typography>
-          {parseInt(
-            channelDetail?.statistics?.subscriberCount
-          ).toLocaleString()}{" "}
-          Subscribers
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString()} Subscribers
+            </Typography>
+          )}
         </CardContent>
       </Link>
     </Box>
